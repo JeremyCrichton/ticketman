22e394fa01fab65f2c5a4f3439a6bfa725cad819
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-04-21 19:34:05 -0700

    update presentation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,14 @@
 class Project < ApplicationRecord
-  has_many :tickets, dependent: :destroy
-
+  has_many :tickets, foreign_key: :project_id, dependent: :destroy
+  
   validates :name, presence: true
   validates :description, presence: true
+
+  def open_tickets
+    self.tickets.size - self.closed_tickets
+  end
+
+  def closed_tickets
+    self.tickets.where('status' => 'fixed').size
+  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,7 @@ class Ticket < ApplicationRecord
 
   belongs_to :project
   belongs_to :creator, foreign_key: :creator_id, class_name: 'User'
-  belongs_to :assignee, foreign_key: :assignee_id, class_name: 'User'
+  belongs_to :assignee, foreign_key: :assignee_id, class_name: 'User', optional: true
   
   has_many :ticket_tags
   has_many :tags, through: :ticket_tags
